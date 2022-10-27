197e60084aab0e761658c3e451c3b3a9fb4644a8
Nick Snow <nicksnowdev@gmail.com>
2022-10-27 18:38:46 -0400

    Consolidate with one-lemon.com version

--- src/sketch.js
+++ src/sketch.js
@@ -9,6 +9,7 @@ const controls = {
   symmetry: true
 }
 let canvas;
+let mousedOver = false;
 let graphics1; // stage 1
 let graphics2; // stage 2
 let graphics3; // rule
@@ -306,10 +307,20 @@ function arrToBuffer(arr, buffer) {
   buffer.updatePixels();
 }
 
+// this is called by the mouseOver event listener on canvas
+function enablePaint() {
+  mousedOver = true;
+}
+// this is called by the mouseOut event listener on canvas
+function disablePaint() {
+  mousedOver = false;
+}
 
 function setup() {
   pixelDensity(1); // account for high-density displays
   canvas = createCanvas(floor(windowHeight * .8 * .5) * 2, floor(windowHeight * .8 * .5) * 2, WEBGL); // 3D mode to allow shaders
+  canvas.mouseOver(enablePaint);
+  canvas.mouseOut(disablePaint);
   canvas.position(0, 10, "relative");
   canvas.style("border-style", "solid");
   canvas.style("border-color", "gray");
@@ -447,7 +458,7 @@ function draw() {
       let paintX = (mouseX - halfWidth) / zoom + halfWidth - panX; // the half vars cancel out at zoom = 1
       let paintY = (mouseY - halfHeight) / zoom + halfHeight - panY;
       let painted = 0;
-      if(!breaker && mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+      if(!breaker && mousedOver) {
         breaker = 1; // prevent running this section 100 times a frame
         // paint the lines
         if(mouseIsPressed && (mouseButton == LEFT || mouseButton == RIGHT)) {
